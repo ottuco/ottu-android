8841e9037de8c9bc98f0736f76da1b928c5b7a41
ihorbenko <61270398+ihorbenko@users.noreply.github.com>
2025-03-17 13:53:07 +0200

    Feature/release 2 (#1)

--- Example/app/src/main/java/com/ottu/network/CreateTransactionRequest.kt
+++ Example/app/src/main/java/com/ottu/network/CreateTransactionRequest.kt
@@ -8,6 +8,23 @@ data class CreateTransactionRequest(
     val type: String,
     val customer_id: String?,
     val customer_phone: String?,
+    val customer_first_name: String?,
+    val customer_last_name: String?,
+    val customer_email: String?,
+    val billing_address: BillingAddress?,
     val include_sdk_setup_preload: Boolean,
+    val card_acceptance_criteria: CardAcceptanceCriteria? = null,
     val language: String
-)
+) {
+
+    data class BillingAddress(
+        val country: String?,
+        val city: String?,
+        val line1: String?,
+    )
+
+    data class CardAcceptanceCriteria(
+        val min_expiry_time: Int
+    )
+
+}
