2878b1b98ab11070c9f4e6c92d4b7e4b139ac378
ihorbenko <ihorbenko@plexteq.com>
2025-09-03 15:15:53 +0300

    Updated SDK version

--- Example/app/build.gradle.kts
+++ Example/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.ottuco:ottu-android-checkout:2.1.2")
+    implementation("com.github.ottuco:ottu-android-checkout:2.1.3")
 
     //FIXME
     implementation(libs.sentry)
